102b75188c0497f437bdc0c9b0ee7df1a76aa7e6
Victoria  Topham <topham.victoria@gmail.com>
2023-01-17 12:52:41 -0800

    Spun up react app

---
+++ crazy-coins/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Home from './components/pages/home'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
